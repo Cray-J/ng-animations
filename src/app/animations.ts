2b9ba3252933f5dc22f8b7b5c10888cc86b3c684
Kristoffer Jensen <kje@computas.com>
2018-04-15 23:13:40 +0200

    Finished with animations

---
+++ src/app/animations.ts
@@ -0,0 +1,123 @@
+import {animate, group, keyframes, state, style, transition, trigger} from '@angular/animations';
+
+
+export const showStateTrigger = trigger('showState', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300)
+  // ]),
+  // transition('* => void', animate(300, style({
+  //   opacity: 0
+  // })))
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(300)
+  ]),
+  transition(':leave', animate(300, style({
+    opacity: 0
+  })))
+]);
+
+export const listStateTrigger = trigger('listState', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300)
+  // ]),
+  // transition('* => void', animate(300, style({
+  //   opacity: 0
+  // })))
+  transition(':enter', [
+    style({
+      opacity: 0,
+      backgroundColor: 'white'
+    }),
+    group([
+      animate(1000, style({
+        opacity: 0.7
+      })),
+      animate('5000ms ease-out', keyframes([
+        style({
+          backgroundColor: 'white',
+          offset: 0
+        }),
+        style({
+          backgroundColor: 'red',
+          offset: 0.8
+        }),
+        style({
+          backgroundColor: 'green',
+          offset: 1
+        })
+      ]))
+    ]),
+    animate(300, style({
+      backgroundColor: 'lightblue'
+    }))
+  ]),
+  transition(':leave', animate(300, style({
+    opacity: 0
+  })))
+]);
+
+
+export const animateStateTrigger = trigger('animateState', [
+  transition('* => *', [
+    animate('4000ms cubic-bezier(.16,.78,0,.92)', style({
+      width: 0
+    })),
+    animate(400, style({
+      width: '*'
+    }))
+  ])
+]);
+
+export const clickedStateTrigger = trigger('clickedState', [
+  state('default', style({
+    backgroundColor: 'orange',
+    width: '100px',
+    height: '100px'
+  })),
+  state('clicked', style({
+    backgroundColor: 'blue',
+    width: '300px',
+    height: '50px'
+  })),
+  state('mousedown', style({
+    backgroundColor: 'red',
+    border: '1px solid black',
+    width: '100px',
+    height: '100px'
+  })),
+  transition('default => clicked', animate('1s 500ms ease-in')),
+  transition('clicked => default', animate(300)),
+  transition('mousedown <=> clicked', animate(300))
+]);
+export const numberEnteredStateTrigger = trigger('numberEnteredState', [
+  state('unselected', style({
+    border: '1px solid black',
+    padding: '5px',
+    backgroundColor: 'white'
+  })),
+  state('selected', style({
+    border: '2px solid blue',
+    padding: '4px',
+    backgroundColor: 'lightBlue'
+  })),
+  transition('unselected => selected', [
+    style({
+      border: '2px solid black',
+      padding: '4px'
+    }),
+    animate('600ms 100ms ease-out', style({
+      backgroundColor: 'red',
+      transform: 'scale(1.05)'
+    })),
+    animate(300)
+  ])
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,58 @@
 import {Component, Renderer2} from '@angular/core';
+import {animateStateTrigger, clickedStateTrigger, listStateTrigger, numberEnteredStateTrigger, showStateTrigger} from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    clickedStateTrigger,
+    numberEnteredStateTrigger,
+    showStateTrigger,
+    animateStateTrigger,
+    listStateTrigger
+  ]
 })
 export class AppComponent {
   isFavorite = false;
+  divClicked = false;
+  doAnimate = false;
+  clickInfo = 'default';
+  paragraphClick = 'default';
+  numberEntered;
+
+  isShown = false;
+  width = 400;
+  animate = false;
+  testResults = [];
 
   constructor(private renderer: Renderer2) {}
+
+  onClickSimple() {
+    this.clickInfo = 'clicked';
+    setTimeout(() => {
+      this.clickInfo = 'default';
+    }, 3000);
+  }
+
+  onAddElement() {
+    this.testResults.push(Math.random());
+  }
+
 
   onShowBoring(element: HTMLElement) {
     this.renderer.setStyle(element, 'display', 'block');
   }
+
+  onAnimationStarted(event: AnimationEvent) {
+    console.log(event);
+  }
+
+  onAnimationDone(event: AnimationEvent) {
+    console.log(event);
+  }
+
+
 
   // courseGoals = [
   //   { title: 'Master Angular Styling', isActiveGoal: true },
